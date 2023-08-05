0cb8fae48ea47cd1afdfc9ce755541572616c2e7
kendrickech <echevken@uw.edu>
2023-08-04 22:49:10 -0700

    Add chart 3

---
+++ third_chart_region.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(ggplot2)
+
+life_expectancy_data <- read.csv('Life-Expectancy-Data-Updated.csv')
+
+
+
+life_expectancy_data <- life_expectancy_data %>% group_by(Region) %>%
+                        summarise(avg_infant_death = mean(Infant_deaths),
+                                  avg_under_five_death = mean(Under_five_deaths),
+                                  avg_adult_death = mean(Adult_mortality))
+
+ggplot(data = life_expectancy_data, aes(x=Region, y=avg_infant_death)) +
+  geom_bar(stat="identity", fill="steelblue") +
+  scale_x_discrete(guide=guide_axis(n.dodge = 2)) +
+  labs(title="Average Infant Death per 1000 in Each Region",
+       x="Region",
+       y="Average Infant Death per 100") +
+  theme(axis.text = element_text(face = "bold"))
+
